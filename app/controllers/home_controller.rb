9c3ae17fdcdb8eb8f657d803fd6293fcd7e8dec8
ken <e.kfc47@gmail.com>
2023-09-13 18:59:33 +0900

    headerのログインボタン表示、headerのavatar表示、ログアウト

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+    # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
     def index
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  def avatar_image
+    # if profile&.avatar&.attached?
+    #   profile.avatar
+    # else
+    'profile01.png'
+    # end
+  end
 end
