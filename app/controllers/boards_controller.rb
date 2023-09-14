5a3adba2413e2539da9eec343e7571e47c90fb03
ken <e.kfc47@gmail.com>
2023-09-15 06:23:23 +0900

    boardのnew（投稿ページ）を作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,19 @@
 class BoardsController < ApplicationController
     # before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+    # boardsにいる状態から、onlyで指定したアクションを実行しようとしたらautheticate_userによってログインページに遷移させられる
+    # =onlyで指定したアクションはログインしていないとできない
+    before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+
     def index
+        # Boardテーブルからすべてを取得
+        @boards = Board.all
     end
+
+    def new
+        @board = Board.new
+    end
+
+
+
 end
