23209773475df057bd6a333b0e9a45aa328fd410
ken <e.kfc47@gmail.com>
2023-09-14 18:53:36 +0900

    boardモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :articles, dependent: :destroy
   
   def avatar_image
     # if profile&.avatar&.attached?
