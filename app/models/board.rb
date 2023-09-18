20c5fde163d4281a11c6da033db7c51b44375623
ken <e.kfc47@gmail.com>
2023-09-19 04:55:42 +0900

    commentの表示と、全体的な修正

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,4 +2,8 @@ class Board < ApplicationRecord
 
   belongs_to :user
 
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :content, presence: true
+  validates :content, uniqueness: true
 end
